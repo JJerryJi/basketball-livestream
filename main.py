a733de6d67ced85dcd71c153dfdf1c189f7f8170
ZIXIANG JI <jerryji040506@ucla.edu>
2023-08-20 22:06:45 -0700

    mimic latency logic

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 import random
 import asyncio
+import gzip, csv, json, time
 
 app = FastAPI()
 
@@ -15,42 +16,60 @@ html = """
     <h2 id="scoreboard">Score: Team A 0 - 0 Team B</h2>
     <h1>Basketball Game Live Stream</h1>
     <ul id='updates'></ul>
-    <form id="message-form">
+    <form id="username-form">
+        <input type="text" id="username-input" placeholder="Enter your username">
+        <button type="submit">Join Chat</button>
+    </form>
+    <form id="message-form" style="display: none;">
         <input type="text" id="message-input" placeholder="Type your message">
-        <input type="text" id="recipient-input" placeholder="Enter recipient's user ID">
+        <input type="text" id="recipient-input" placeholder="Enter recipient's username">
         <button type="submit">Send</button>
     </form>
     <script>
-        var ws = new WebSocket("ws://localhost:8000/ws");
-        ws.onmessage = function(event) {
-            var updates = document.getElementById('updates');
-            var update = document.createElement('li');
-            var content = document.createTextNode(event.data);
-            update.appendChild(content);
-            updates.appendChild(update);
-
-            if (event.data.startsWith("Scores:")) {
-                document.getElementById("scoreboard").textContent = event.data;
-            }
-        };
+        var usernameForm = document.getElementById("username-form");
+        var usernameInput = document.getElementById("username-input");
 
         var messageForm = document.getElementById("message-form");
         var messageInput = document.getElementById("message-input");
         var recipientInput = document.getElementById("recipient-input");
 
-        messageForm.addEventListener("submit", function(event) {
+        usernameForm.addEventListener("submit", function(event) {
             event.preventDefault();
+            var username = usernameInput.value;
+            usernameForm.style.display = "none"; // Hide the username form
+            messageForm.style.display = "block"; // Show the message form
 
-            var message = messageInput.value;
-            var recipient = recipientInput.value;
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onopen = function() {
+                ws.send(`${username}`); // Send the chosen username to the server
+            };
 
-            if (recipient) {
-                message = `/private ${recipient} ${message}`;
-            }
+            ws.onmessage = function(event) {
+                var updates = document.getElementById('updates');
+                var update = document.createElement('li');
+                var content = document.createTextNode(event.data);
+                update.appendChild(content);
+                updates.appendChild(update);
 
-            messageInput.value = "";
-            recipientInput.value = "";
-            ws.send(message);
+                if (event.data.startsWith("Scores:")) {
+                    document.getElementById("scoreboard").textContent = event.data;
+                }
+            };
+
+            messageForm.addEventListener("submit", function(event) {
+                event.preventDefault();
+
+                var message = messageInput.value;
+                var recipient = recipientInput.value;
+
+                if (recipient) {
+                    message = `/private ${recipient} ${message}`;
+                }
+
+                messageInput.value = "";
+                recipientInput.value = "";
+                ws.send(message);
+            });
         });
     </script>
 </body>
@@ -78,25 +97,26 @@ class GameManager:
         for websocket in self.connected_websockets.values():
             await websocket.send_text(message)
 
-    async def send_new_user_message(self, user_id: str):
-        await self.broadcast(f"Welcome, User {user_id}! You are now connected to the game.")
+    async def send_new_user_message(self, username: str):
+        await self.broadcast(f"Welcome, {username}! You are now connected to the game.")
+        await self.connected_websockets[username].send_text(f'Your username is: {username}')
 
-    async def send_private_message(self, sender_id, recipient_id, message):
-        sender_socket = self.connected_websockets.get(sender_id)
-        recipient_socket = self.connected_websockets.get(recipient_id)
+    async def send_private_message(self, sender_username, recipient_username, message):
+        sender_socket = self.connected_websockets.get(sender_username)
+        recipient_socket = self.connected_websockets.get(recipient_username)
 
         if sender_socket and recipient_socket:
-            chat_id = f"{sender_id}-{recipient_id}"
+            chat_id = f"{sender_username}-{recipient_username}"
             self.private_chats.setdefault(chat_id, [])
-            self.private_chats[chat_id].append((sender_id, message))
+            self.private_chats[chat_id].append((sender_username, message))
             await self.broadcast_private_message(chat_id, sender_socket, recipient_socket, message)
         else:
-            await self.broadcast(f"User {sender_id}: Private message failed, user not found.")
+            await self.broadcast(f"{sender_username}: Private message failed, user not found.")
 
     async def broadcast_private_message(self, chat_id, sender_socket, recipient_socket, message):
         chat_history = self.private_chats.get(chat_id, [])
         for user_id, msg in chat_history:
-            await sender_socket.send_text(f"To {chat_id.split('-')[0]}: {msg}")
+            await sender_socket.send_text(f"To {chat_id.split('-')[1]}: {msg}")
             await recipient_socket.send_text(f"From {user_id}: {msg}")
 
 manager = GameManager()
@@ -109,24 +129,50 @@ async def get():
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
 
-    user_id = str(random.randint(1, 1000))
-    manager.connected_websockets[user_id] = websocket
+    username = await websocket.receive_text()
+    manager.connected_websockets[username] = websocket
 
-    await manager.send_new_user_message(user_id)
+    await manager.send_new_user_message(username)
 
     try:
         while True:
+            # take some time to performa a request
+            convert_json_takes_time()
             message = await websocket.receive_text()
             if message.startswith("/private"):
                 _, recipient, private_message = message.split(" ", 2)
-                await manager.send_private_message(user_id, recipient, private_message)
+                await manager.send_private_message(username, recipient, private_message)
             else:
-                await manager.broadcast(f"User {user_id}: {message}")
+                await manager.broadcast(f"User {username}: {message}")
     except WebSocketDisconnect:
-        del manager.connected_websockets[user_id]
+        del manager.connected_websockets[username]
         users = list(manager.connected_websockets.keys())
         await manager.broadcast(f"UpdateRecipients:{','.join(users)}")
+
 
 @app.on_event("shutdown")
 async def shutdown():
     manager.game_running = False
+
+
+def convert_json_takes_time():
+    json_output_path = "/Users/jerry/Desktop/physionet.org/files/mimiciv/2.0/icu/procedureevents.json"
+    csv_file_path = "/Users/jerry/Desktop/physionet.org/files/mimiciv/2.0/icu/procedureevents.csv.gz"
+
+    print("JSON conversion started")
+    start_time = time.time()  # Record the start time
+
+    json_data = []
+    with gzip.open(csv_file_path, 'rt') as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        for row in csvreader:
+            json_data.append(row)
+
+    with open(json_output_path, 'w') as jsonfile:
+        json.dump(json_data, jsonfile, indent=4)
+
+    end_time = time.time()  # Record the end time
+    elapsed_time = end_time - start_time
+    print(f"JSON conversion completed in {elapsed_time:.2f} seconds")
+
+    print("Conversion completed!")
